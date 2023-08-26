4b19561af0a30ba122e370d826a7adb886489f93
Akmat <88925317+curt-man@users.noreply.github.com>
2023-08-26 20:53:35 +0600

    Added new Class Library Project
    Realized that I have a lot of repetitive code and decided to move it to shared project.

--- HealthCampus.CommonUtilities/Utilities/MaxFileSizeAttribute.cs
+++ HealthCampus.CommonUtilities/Utilities/MaxFileSizeAttribute.cs
@@ -1,6 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
 
-namespace HealthCampus.Services.AppFileAPI.Utilities
+namespace HealthCampus.CommonUtilities
 {
     public class MaxFileSizeAttribute : ValidationAttribute
     {
